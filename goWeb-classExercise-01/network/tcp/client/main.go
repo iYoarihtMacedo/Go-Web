e6dfa683689caf21dce956a02f82fc239f0a3a9c
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-10 17:06:39 -0600

    feat: class exercise for tcp

---
+++ goWeb-classExercise-01/network/tcp/client/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	// Connect to server
+	conn, err := net.Dial("tcp", "localhost:2000")
+	if err != nil {
+		fmt.Println(err)
+		return
+
+	}
+	defer conn.Close()
+
+	// listen to reply
+	go func() {
+		for {
+			// read bytes from connection
+			buf := make([]byte, 1024)
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			data := string(buf[:n])
+			fmt.Println("Server reply:", data)
+
+		}
+	}()
+
+	// Run for loop forever (or until ctrl c)
+	for {
+		// Read input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		text, _ := reader.ReadString('\n')
+
+		// Send to server
+		_, err := conn.Write([]byte(text))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+}
