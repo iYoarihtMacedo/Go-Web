96e6abd48332611f354e0eaf7537a9b2bc4bc381
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-10 21:01:29 -0600

    feat: decode notes

---
+++ goWeb-classExercise-01/json/decoding/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type Person struct {
+	Name string   `json.name`
+	Age  int      `json.age`
+	Cars []string `json.cars`
+}
+
+func main() {
+
+	// strings reader
+	reader := strings.NewReader(`{"name":"John", "age":30, "cars":["Ford", "BMW", "Fiat"]}`)
+
+	dc := json.NewDecoder(reader)
+
+	// decode into person struct
+	var p Person
+	if err := dc.Decode(&p); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(p)
+
+}
