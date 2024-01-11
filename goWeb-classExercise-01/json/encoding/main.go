040d3ed6eb2655aaa6e173dffc5a146a6342dda9
iYoarihtMacedo <irvin.macedo@mercadolibre.com.mx>
2024-01-10 21:01:43 -0600

    feat: encode notes

---
+++ goWeb-classExercise-01/json/encoding/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Person struct {
+	Name string   `json.name`
+	Age  int      `json.age`
+	Cars []string `json.cars`
+}
+
+func main() {
+
+	// create person struct
+	p := Person{
+		Name: "John",
+		Age:  30,
+		Cars: []string{"Ford", "BMW", "Fiat"},
+	}
+
+	// create encoder
+	ec := json.NewEncoder(os.Stdout)
+	if err := ec.Encode(p); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
